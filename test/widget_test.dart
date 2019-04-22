a3f4ef2c1b58f0ff89e5e3e3036e9b82adcf8a8d
강동윤 <kdy1@outlook.kr>
2019-04-22 20:48:50 +0900

    Test widget initialization

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:var_widget/var_widget.dart';
 
 void main() {
   group('VarWidget', _tests);
 }
 
-void _tests() {}
+void _tests() {
+  group('VarWidget', () {
+    Var<String> v;
+
+    setUp(() {
+      v = new Var('');
+    });
+
+    tearDown(() {
+      v.close();
+    });
+
+    testWidgets('works', (WidgetTester tester) async {
+      await tester.pumpWidget(_render(v));
+
+      expect(
+        find.text(''),
+        findsOneWidget,
+      );
+
+      v.value = 'foo';
+
+      expect(
+        find.text(''),
+        findsOneWidget,
+        reason: 'should not be updated yet',
+      );
+
+      await tester.runAsync(() => Future.value(null));
+      await tester.pump();
+
+      expect(
+        find.text(''),
+        findsNothing,
+      );
+
+      expect(
+        find.text('foo'),
+        findsOneWidget,
+      );
+    });
+  });
+
+  group('VarBuilder', () {});
+}
+
+Widget _render(Value<String> v) {
+  return MaterialApp(
+    home: _VarText(value: v),
+  );
+}
+
+class _VarText extends VarWidget<String> {
+  const _VarText({
+    Key key,
+    @required Value<String> value,
+  }) : super(
+          key: key,
+          value: value,
+        );
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(value);
+  }
+}
